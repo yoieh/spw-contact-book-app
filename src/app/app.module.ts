65783c6f5ad76cd8de5373bb56563565081e7950
yoieh <yoieh@live.se>
2017-10-24 21:29:25 +0200

    added last commits to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { ContactsComponent } from './routes/contacts/contacts.component';
+import { ContactComponent } from './routes/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
