64fd7f38478224f1638c8ebb311c067f1641d3d9
yoieh <yoieh@live.se>
2017-10-24 21:29:55 +0200

    added a core module and core routing system

---
+++ src/app/core/core.routing.ts
@@ -0,0 +1,21 @@
+// This is the routing file and all page routes sould go here
+import { HomeComponent } from '../routes/home/home.component';
+import { ContactsComponent } from '../routes/contacts/contacts.component';
+import { ContactComponent } from '../routes/contact/contact.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Routs array
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'contacts/:id', component: ContactComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule] // export routs to use in other modules
+})
+export class CoreRoutingModule { }
