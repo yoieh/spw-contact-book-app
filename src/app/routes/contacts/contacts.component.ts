6e7400e4dccbf30afa164ae79bef242c7a8c7a1d
yoieh <yoieh@live.se>
2017-10-26 06:28:41 +0200

    added list of contacts

--- src/app/routes/contacts/contacts.component.ts
+++ src/app/routes/contacts/contacts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ContactService } from '../../services/contact.service';
+import { Contact } from '../../interfaces/contact';
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  public keysOfContact;
 
-  constructor() { }
+  @Input() contacts: Contact;
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
+    this.list().then(contacts => this.getkeys(contacts));
   }
 
+  // Getts all keys of retrevide sites for easyer lopping list of cards
+  getkeys(contacts: Contact): Promise<string[]> {
+    return new Promise((resolve) =>
+      resolve(this.keysOfContact = Object.keys(contacts)));
+  }
+
+  // Gets list of sites from sitesSerivce
+  list(): Promise<Contact> {
+    return this.contactService.get()
+      .then((res: Contact) => this.contacts = res);
+  }
 }
