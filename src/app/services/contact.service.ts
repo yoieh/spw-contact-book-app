6b756039022f9d1862383377db2ffa7352a13711
yoieh <yoieh@live.se>
2017-10-31 19:26:54 +0100

    Added new methode to finde one contact

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,22 +1,46 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { ContactResults } from '../interfaces/contact';
-import { SearchContactsPipe } from '../pipes/search-contacts.pipe';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  HttpClient,
+  HttpParams
+} from '@angular/common/http';
+import {
+  ContactResults
+} from '../interfaces/contact';
+import {
+  SearchContactsPipe
+} from '../pipes/search-contacts.pipe';
+import {
+  Contact
+} from './../interfaces/contact';
 
 @Injectable()
 export class ContactService {
   private api = 'https://randomuser.me/api/';
   private seed = '?seed=5294e1f64f0126bd';
 
-  constructor(private Http: HttpClient) {
+  constructor(private Http: HttpClient) {}
+
+  // Used find a contact by id.value
+  find(id: any) {
+    return new Promise((resolve, reject) => {
+      return this.get().then((res: ContactResults) => {
+        return res.results.map(contact => {
+          if (contact.id.value === id) {
+            resolve(contact);
+          }
+        });
+      });
+    });
   }
 
+  // Getting all contacts
   get() {
     return new Promise((resolve, reject) => {
-      this.Http.get<ContactResults>(this.api + this.seed + '&results=500&nat=us,dk,fr,gb&inc=gender,name,email,phone,picture,id,location')
-      .subscribe(res => {
-        resolve(res);
-      });
+      return this.Http.get < ContactResults > (this.api + this.seed +
+          '&results=500&nat=us,dk,fr,gb&inc=gender,name,email,phone,cell,picture,id,location')
+        .subscribe(res => resolve(res));
     });
   }
 
